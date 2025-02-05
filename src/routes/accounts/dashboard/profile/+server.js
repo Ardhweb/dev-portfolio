2159d5f017c04e0c62ac433125fe56c9e0a9ebf8
Ardhweb <sun.projectsdev@gmail.com>
2025-02-05 17:23:09 +0530

    Update +server.js

--- src/routes/accounts/dashboard/profile/+server.js
+++ src/routes/accounts/dashboard/profile/+server.js
@@ -1,12 +1,12 @@
 // src/routes/dashboard/+page.server.js
-export const load = async ({event }) => {
-    const session   = await event.locals.auth()
-    if (!session) {
-      throw redirect(302, "/accounts/auth/login");
-    }
+// export const load = async ({event }) => {
+//     const session   = await event.locals.auth()
+//     if (!session) {
+//       throw redirect(302, "/accounts/auth/login");
+//     }
   
-    return {
-      session: await event.locals.auth(),
-    };
-  };
+//     return {
+//       session: await event.locals.auth(),
+//     };
+//   };
   
